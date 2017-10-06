0bfd76320c001772137ccd7c8f51a2179cd6a44d
facundoalarcon <facundoalarcon93@gmail.com>
2017-10-06 19:33:05 -0300

    version 2: no anda

--- conf_genRules_apGenRules.py
+++ conf_genRules_apGenRules.py
@@ -1,43 +1,50 @@
-def confianzaMinima (transacciones, itemsf, minConf):
-    faux.append(itemsf[1])
-    faux.extend(itemsf[2])#uno las dos partes asi me genera una sola lista con todos los elementos, itemsf es una lista de dos elementos, el antecedente y el consecuente
+def confianzaMinima(transacciones, f, reglaCandidata, minConf):
+    #faux.append(reglaCandidata[1])
+    #faux.extend(reglaCandidata[2])#uno las dos partes asi me genera una sola lista con todos los elementos, itemsf es una lista de dos elementos, el antecedente y el consecuente
+    antecedente = reglaCandidata[1]
     fcont = 0 #contador f
     antcont = 0 #contador antecedente de itemsf o sea itemsf[1]
     for transaccion in transacciones:
         cont = 0 #renuevo por cada transaccion
-        for item in aux:
+        for item in f:
             if item in transaccion:
                 cont += 1 #voy sumando la cantidad de veces que coinciden los elementos en la transaccion individual
-                if cont == len(faux): #si coinciden todos le sumo 1 ya que esos elementos aparecen en la transaccion
-                    fcont += 1
-            if item in itemsf[1]: #quiere decir que el item de f tambien es un antecedente
-                antcont +=1
-    confianza = fcont/antcont
-    if confianza >= minConf:
-        return True
-    return False
+            if cont == len(f): #si coinciden todos le sumo 1 ya que esos elementos aparecen en la transaccion
+                fcont += 1
+            if (item in antecedente) and (item in transaccion): #quiere decir que el item de f tambien es un antecedente
+                antcont += 1
+    if antcont != 0:#quitar despues
+        confianza = fcont/antcont
+        if confianza >= minConf:
+            return True
+        return False
+    else: return False
 
-def genRules (transacciones, itemsFrecuentes, minSup, minConf,k):
+def genRules (transacciones, itemsFrecuentes, minSup, minConf):
 #la idea del codigo anterior es [1,2,3] toma primero el 1 lo agrega como consecuente y lo que queda de la lista es precedente, asi va haciendo en el bucle
-    if k >= 2:
-        reglas = []
-        hh1 = []
-        for f in itemsFrecuentes:
+    reglas = []
+    hh1 = []
+    for f in itemsFrecuentes:
+        itemSet = f[0]
+        k = len(f[0])
+        if k >= 2:
+            aux = itemSet#tiene todos los elementos
             regla = []
-            aux = f
-            aux2 = []
-            for item in f:
+            for item in itemSet:
                 aux.remove(item)
-                aux2.append([aux])
-                aux2.append([item])
-                if confianzaMinima(transacciones, aux2, minConf):#no se definio
-                    regla.append(aux2[1])
-                    regla.append(aux2[2])
-                    hh1.append(item)
-                    reglas.append(regla)
-            reglas.extend(apGenRules(f, hh1, k, m = 1)) #concateno la lista de un elemento con la de mas de un elemento
-    return reglas
+                antecedentes = aux
+                consecuente = [item]
+                regla.append(antecedentes)
+                regla.append(consecuente)
+                #antecedentes y un consecuente, aux ya es una lista
+                #if (confianzaMinima(transacciones, f, aux2, minConf)):
 
+                reglas.append(regla)
+                #    hh1.append(item)
+
+        #reglas.extend(apGenRules(f, hh1, k, m = 1)) #concateno la lista de un elemento con la de mas de un elemento
+    return reglas
+'''
     def apGenRules(f, hh, k, m):
         if (k > (m + 1)) and (len(hh) != 0):
             hhx = candidateGen(hh)
@@ -64,11 +71,16 @@ def genRules (transacciones, itemsFrecuentes, minSup, minConf,k):
                     cont += 1
                     cont2 += 1
             return reglas
-
+'''
 if __name__=="__main__":
-    transacciones = [[1,3],[1,4],[2,3,4],[1,2],[1]]
-    itemsFrecuentes = [[1],[2],[3],[4],[1,2],[1,3],[1,4],[2,3],[2,4],[3,4],[1,2,3],[1,2,4],[1,3,4],[2,3,4],[1,2,3,4]]
-    k=2
-    minSup = 0.8
-    minConf = 0.9*5
-    print (genRules(transacciones, itemsFrecuentes, minSup, minConf, k))
+    transacciones = [[1,2,3,4],[3,4],[2,4],[1,2],[1,2],[1,2,3,4]]
+    #ff [[listaElementos], soporte]
+    #confianza = soportef/cantAntecedente
+    #ff = [[1],4],[[2],5],[[3],3],[[4],4],[[1,2],4],[[1,3],2],[[1,4],2],[[2,3],2],[[2,4],3],[[3,4],3],[[1,2,3],2],[[1,2,4],2],[[1,3,4],2],[[2,3,4],2],[[1,2,3,4],2]]
+    #ff = [[1,2],4],[[1,3],2],[[1,4],2],[[2,3],2],[[2,4],3],[[3,4],3]
+    #ff = [[1,2,3],2],[[1,2,4],2],[[1,3,4],2],[[2,3,4],2]
+    ff = [[1,2,3,4],2]]
+    minSup = 0.2
+    minConf = 0.3*6
+    r = genRules(transacciones, ff, minSup, minConf)
+    print (r)
